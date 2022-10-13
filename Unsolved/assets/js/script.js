49a4b07d99bdb2eed0905b015f33fb0fdcaad854
Tom Bond <tombond248@gmail.com>
2022-10-13 20:27:20 +0100

    initial files

--- Unsolved/assets/js/script.js
+++ Unsolved/assets/js/script.js
@@ -1,6 +1,8 @@
 // TODO: Which element is the following line of code selecting?
+//Carousel element
 var carousel = document.querySelector(".carouselbox");
 // TODO: Which element is the following line of code selecting?
+// the button next 
 var next = carousel.querySelector(".next");
 var prev = carousel.querySelector(".prev");
 var index = 0;
@@ -27,21 +29,26 @@ function navigate(direction) {
 }
 
 // TODO: Describe the functionality of the following event listener.
+//clicking an image takes you to a new window with a different image 
 carousel.addEventListener("click", function() {
   window.location.href = images[index];
 });
 
 // TODO: Describe the functionality of the following event listener.
+// clicking next displays next image in carousel
 next.addEventListener("click", function(event) {
   // TODO: What is the purpose of the following line of code?
+  // stops new window from opening
   event.stopPropagation();
 
   navigate(1);
 });
 
 // TODO: Describe the functionality of the following event listener.
+// clicking previous displays previous image 
 prev.addEventListener("click", function(event) {
     // TODO: What would happen if we didn't add the following line of code?
+    // a new window would open due to bubbling 
   event.stopPropagation();
 
   navigate(-1);
